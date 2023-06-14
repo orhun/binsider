6a4e9d222783298313b1295163470bf598d4d529
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-06-14 20:07:31 +0300

    style(elf): show header similar to readelf command

--- src/elf/header.rs
+++ src/elf/header.rs
@@ -21,7 +21,23 @@ impl Display for Header {
         writeln!(f, "Endianness: {:?}", self.inner.endianness)?;
         writeln!(
             f,
-            "Object Type: {}",
+            "Version: {}",
+            match self.inner.version {
+                1 => String::from("1 (current)"),
+                v => v.to_string(),
+            }
+        )?;
+        writeln!(
+            f,
+            "OS/ABI: {}",
+            e_osabi_to_string(self.inner.osabi)
+                .strip_prefix("ELFOSABI_")
+                .unwrap_or("unknown")
+        )?;
+        writeln!(f, "ABI Version: {}", self.inner.abiversion)?;
+        writeln!(
+            f,
+            "Type: {}",
             match e_type_to_human_str(self.inner.e_type) {
                 Some(s) => s.to_string(),
                 None => format!("e_type({:#x})", self.inner.e_type),
@@ -35,24 +51,31 @@ impl Display for Header {
                 None => format!("e_machine({:#x})", self.inner.e_machine),
             }
         )?;
-        writeln!(f, "OS/ABI: {}", e_osabi_to_string(self.inner.osabi))?;
         writeln!(f, "Entry point address: {:#x}", self.inner.e_entry)?;
         writeln!(
             f,
-            "Start of program headers: {:#x} (bytes into file)",
+            "Start of program headers: {} (bytes into file)",
             self.inner.e_phoff
         )?;
         writeln!(
             f,
-            "Start of section headers: {:#x} (bytes into file)",
+            "Start of section headers: {} (bytes into file)",
             self.inner.e_shoff
         )?;
         writeln!(f, "Flags: {:#x}", self.inner.e_flags)?;
-        writeln!(f, "Size of this header: {:#x}", self.inner.e_ehsize)?;
-        writeln!(f, "Size of program header: {:#x}", self.inner.e_phentsize)?;
-        writeln!(f, "Number of program headers: {:#x}", self.inner.e_phnum)?;
-        writeln!(f, "Size of section header: {:#x}", self.inner.e_shentsize)?;
-        writeln!(f, "Number of section headers: {:#x}", self.inner.e_shnum)?;
+        writeln!(f, "Size of this header: {} (bytes)", self.inner.e_ehsize)?;
+        writeln!(
+            f,
+            "Size of program header: {} (bytes)",
+            self.inner.e_phentsize
+        )?;
+        writeln!(f, "Number of program headers: {}", self.inner.e_phnum)?;
+        writeln!(
+            f,
+            "Size of section headers: {} (bytes)",
+            self.inner.e_shentsize
+        )?;
+        writeln!(f, "Number of section headers: {}", self.inner.e_shnum)?;
         writeln!(
             f,
             "Section headers string table section index: {}",
