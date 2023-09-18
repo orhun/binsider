bd47252476d21228a1fd0f4c150fafaf049d043b
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-09-18 14:30:09 +0300

    style(tui): add block to minimum length text

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -306,15 +306,15 @@ pub fn render_strings<B: Backend>(state: &mut State, frame: &mut Frame<'_, B>, r
             state.list.items.len()
         ),
     );
-    let min_length_text = format!("Min length: {}", state.analyzer.strings_len);
-    let selection_text_width = u16::try_from(min_length_text.width()).unwrap_or_default();
-    if let Some(horizontal_area_width) = rect.width.checked_sub(selection_text_width + 3) {
+    let min_length_text = format!("Minimum length: {}", state.analyzer.strings_len);
+    let selection_text_width = u16::try_from(min_length_text.width()).unwrap_or_default() + 2;
+    if let Some(horizontal_area_width) = rect.width.checked_sub(selection_text_width + 2) {
         let vertical_area = Layout::default()
             .direction(Direction::Vertical)
             .constraints(
                 [
                     Constraint::Min(1),
-                    Constraint::Min(1),
+                    Constraint::Min(3),
                     Constraint::Min(rect.height),
                 ]
                 .as_ref(),
@@ -327,15 +327,19 @@ pub fn render_strings<B: Backend>(state: &mut State, frame: &mut Frame<'_, B>, r
                     Constraint::Min(horizontal_area_width),
                     Constraint::Min(selection_text_width),
                     Constraint::Min(1),
-                    Constraint::Min(1),
                 ]
                 .as_ref(),
             )
             .split(vertical_area[1]);
         frame.render_widget(Clear, horizontal_area[1]);
-        frame.render_widget(Paragraph::new(min_length_text), horizontal_area[1]);
-        frame.render_widget(Clear, horizontal_area[2]);
-        frame.render_widget(Paragraph::new(Text::default()), horizontal_area[2]);
+        frame.render_widget(
+            Paragraph::new(min_length_text).block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .border_style(Style::default().fg(Color::Black)),
+            ),
+            horizontal_area[1],
+        );
     }
 }
 
