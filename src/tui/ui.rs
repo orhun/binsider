d6761fdc671d157273d1ed0daf10a82ba9ad93e6
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-09-30 12:07:32 +0300

    fix(ui): avoid crashing when logo does not fit the terminal

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -80,17 +80,19 @@ pub fn render(state: &mut State, frame: &mut Frame) {
     if !state.logo.is_rendered && state.analyzer.files.len() == 1 {
         let area = frame.area();
         let (logo_width, logo_height) = state.logo.get_size();
-        frame.render_widget(
-            &state.logo,
-            Rect::new(
-                area.width / 2 - logo_width / 2,
-                area.height / 2 - logo_height / 2,
-                logo_width,
-                logo_height,
-            ),
-        );
-        state.logo.is_rendered = state.logo.init_time.elapsed().as_millis() > 500;
-        return;
+        if logo_width < area.width && logo_height < area.height {
+            frame.render_widget(
+                &state.logo,
+                Rect::new(
+                    area.width / 2 - logo_width / 2,
+                    area.height / 2 - logo_height / 2,
+                    logo_width,
+                    logo_height,
+                ),
+            );
+            state.logo.is_rendered = state.logo.init_time.elapsed().as_millis() > 500;
+            return;
+        }
     }
     let chunks = Layout::new(
         Direction::Vertical,
