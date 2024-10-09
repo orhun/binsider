b998cdedbe3f51d6d5b4e8f1b466bc78da86c289
ch4og <mitanick@ya.ru>
2024-10-09 19:42:48 +0300

    chore(flake): add binsider-debug package for CI

--- flake.nix
+++ flake.nix
@@ -8,30 +8,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        binsider' = { buildType }: pkgs.rustPlatform.buildRustPackage {
+          name = "binsider";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          checkType = "debug";
+          inherit buildType;
+          checkFlags = [
+            "--skip=test_extract_strings"
+            "--skip=test_init"
+          ];
+          meta = with pkgs.lib; {
+            description = "Analyze ELF binaries like a boss";
+            homepage = "https://binsider.dev/";
+            license = [ licenses.mit licenses.asl20 ];
+          };
+        };
       in
       rec {
         packages = rec {
-          binsider = pkgs.rustPlatform.buildRustPackage {
-            name = "binsider";
-            src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-            checkType = "debug";
-            checkFlags = [
-              "--skip=test_extract_strings"
-              "--skip=test_init"
-            ];
-            meta = with pkgs.lib; {
-              description = "Analyze ELF binaries like a boss";
-              homepage = "https://binsider.dev/";
-              license = [ licenses.mit licenses.asl20 ];
-            };
-          };
+          binsider = binsider' { buildType = "release"; };
+          binsider-debug = binsider' { buildType = "debug"; };
           default = binsider;
         };
-        checks.check = packages.binsider;
+        checks.check = packages.binsider-debug;
+
       }
     );
-
 }
